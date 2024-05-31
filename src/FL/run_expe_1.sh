05a1d5d6b3dca9adde929f5c2ead2b18585e6b5b
serenalucca <serena.lucca@uclouvain.be>
2024-05-31 08:46:15 +0200

    update

--- src/FL/run_expe_1.sh
+++ src/FL/run_expe_1.sh
@@ -12,36 +12,13 @@ sleep 60 # Sleep for 3s to give the server enough time to start
 
 echo "Starting CE server"
 python3 ./FL/fl_ce_server.py --enc --nclients=${nclients} --filepath=${filepath} --dataset=${dataset}&
-sleep 3
 
 for ((i=0; i<nclients; i++)); do
     echo "Starting client $i"
     python3 ./FL/fl_client_enc.py --nclients=${nclients} --partition=${i} --filepath=${filepath} --dataset=${dataset}&
-    sleep 2
 done
     
 # This will allow you to use CTRL+C to stop all background processes
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM
 # Wait for all background processes to complete
 wait
-
-
-echo "Starting server"
-python3 ./FL/fl_server.py --nrounds=${nrounds} --nclients=${nclients} --filepath=${filepath} --dataset=${dataset} --methodo=${methodo} --threshold=${threshold}&
-sleep 60  # Sleep for 3s to give the server enough time to start
-
-echo "Starting CE server"
-python3 ./FL/fl_ce_server.py --nclients=${nclients} --filepath=${filepath} --dataset=${dataset}&
-sleep 2
-for ((i=0; i<nclients; i++)); do
-    echo "Starting client $i"
-    python3 ./FL/fl_client.py --nclients=${nclients} --partition=${i} --filepath=${filepath} --dataset=${dataset}&
-    sleep 1
-done
-
-# This will allow you to use CTRL+C to stop all background processes
-trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM
-# Wait for all background processes to complete
-wait
-
-
